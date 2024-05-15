49472faf3dc60fd5bc9336ede37b94119dd2667d
Neelam <neelam_669@yahoo.com>
2024-05-15 05:06:41 -0700

    Amend

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export const getStatus = (req, res) => {
-  try {
-    const redis = redisClient.isAlive();
-    const db = dbClient.isAlive();
-
-    res.status(200).send({ redis, db });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'Server error' });
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
   }
-};
 
-export const getStats = async (req, res) => {
-  try {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-
-    res.status(200).send({ users, files });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'Server error' });
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
-};
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import auth from '../utils/auth';
-import { getStatus, getStats } from '../controllers/AppController';
-import { postNew, getMe } from '../controllers/UsersController';
-import { getConnect, getDisconnect } from '../controllers/AuthController';
-import { postUpload } from '../controllers/FilesController';
+// eslint-disable-next-line no-unused-vars
+import { Express } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
 
-const router = express.Router();
-
-// AppController routes
-
-// returns true if Redis is alive and if the DB is alive too
-router.get('/status', getStatus);
-// returns the number of users and files in DB
-router.get('/stats', getStats);
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
+const injectRoutes = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
